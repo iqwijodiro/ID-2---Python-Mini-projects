ac3d7f9587c4ceb4d0467c207b0863d07ca377c0
Winder Díaz <winderdiaz29@gmail.com>
2023-06-21 15:08:50 -0400

    comments added

--- movies_rating/app.py
+++ movies_rating/app.py
@@ -1,22 +1,23 @@
 import requests
 
 def movie_rating(title):
-    key = 'a50bd5c'
+    key = 'https://www.omdbapi.com/apikey.aspx' #solicita tu key en este enlace
     endpoint = f'http://www.omdbapi.com/?apikey={key}&t={title}'
 
     resp = requests.get(endpoint)
     data = resp.json()
 
+    # Validamos que la respuesta de la api sea correcta
     if data['Response'] == 'True' and resp.status_code == 200:
-        rating_points = data['imdbRating']
+        rating_points = data['imdbRating'] # Asignamos la propiedad imdbRating al puntaje que manejaremos y lo devolvemos
         return rating_points
     else:
         print('No se encontró esa pelicula')
         return None
 
 def main():
-    title = input('Escribe el nombre de la pelicula que quieres conocer el rating: ')
-    rating_points = movie_rating(title)
+    title = input('Escribe el nombre de la pelicula que quieres conocer el rating: ') #Recibimos del usuario el titulo de la pelicula
+    rating_points = movie_rating(title) #Se lo pasamos como parametro a la funcion de consulta
 
     if rating_points is not None:
         print(f'La pelicula {title} tiene un rating de {rating_points}')
